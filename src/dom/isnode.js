1018d3f6ff441b6d5514976d8025b29ac22523b4
Jaša Mihelčič <jasa.mihelcic@spartan.si>
2019-01-14 11:21:17 +0100

    Implement change for isnode function to work with grapesjs

--- src/dom/isnode.js
+++ src/dom/isnode.js
@@ -13,14 +13,38 @@
  * @param {*} obj
  * @returns {Boolean}
  */
+// Custom build
 export default function isNode( obj ) {
 	if ( obj ) {
-		if ( obj.defaultView ) {
+		if (obj.defaultView) {
+			if (obj.defaultView.frameElement != null) {
+				if (obj.defaultView.frameElement.className.includes('gjs-frame')) {
+					// return obj instanceof obj.defaultView.frameElement.ownerDocument.defaultView.Document;
+					return obj instanceof obj.defaultView.Document;
+				}
+			}
 			return obj instanceof obj.defaultView.Document;
-		} else if ( obj.ownerDocument && obj.ownerDocument.defaultView ) {
+		} else if (obj.ownerDocument && obj.ownerDocument.defaultView) {
+			if (obj.ownerDocument.defaultView.frameElement != null) {
+				if (obj.ownerDocument.defaultView.frameElement.className.includes('gjs-frame')) {
+					return obj instanceof obj.ownerDocument.defaultView.frameElement.ownerDocument.defaultView.Node;
+				}
+			}
 			return obj instanceof obj.ownerDocument.defaultView.Node;
 		}
 	}
 
 	return false;
 }
+
+// export default function isNode( obj ) {
+// 	if ( obj ) {
+// 		if ( obj.defaultView ) {
+// 			return obj instanceof obj.defaultView.Document;
+// 		} else if ( obj.ownerDocument && obj.ownerDocument.defaultView ) {
+// 			return obj instanceof obj.ownerDocument.defaultView.Node;
+// 		}
+// 	}
+//
+// 	return false;
+// }
